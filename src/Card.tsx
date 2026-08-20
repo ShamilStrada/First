4abf2287b87f50c2d9de7f75c53dde14b9847ffc
ShamilStrada <5000mmr02@mail.ru>
2026-08-20 15:42:02 +0500

    Сделано для запоминания

--- src/Card.tsx
+++ src/Card.tsx
@@ -8,6 +8,7 @@ const {id} = useParams() as {id:string};
       <Link to={"/"}><h1>{id}</h1></Link>
       <h2>В силу вступили новые изменения</h2>
       <h2>Для теста</h2>
+      <h2>For new changes for me</h2>
       <h3>Еще парочка изменений</h3>
       <h4>Эти изменения он не увидит</h4>
     </>
