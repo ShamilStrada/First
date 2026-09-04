aa8d94e534ae4e9cc64b7aa1a9242489d8ab4be6
ShamilStrada <5000mmr02@mail.ru>
2026-09-04 16:07:35 +0500

    Revert "Создание второй тестовой ветки"

--- src/Card.tsx
+++ src/Card.tsx
@@ -9,8 +9,7 @@ export function MyCard() {
       <Link to={getAllRoutes()}>
         <h1>{numberOne}</h1>
       </Link>
-      <h2>В силу нихуя не вступило</h2>
-      <h3>GGmoll</h3>
+      <h2>В силу вступили новые изменения</h2>
     </>
   );
 }
